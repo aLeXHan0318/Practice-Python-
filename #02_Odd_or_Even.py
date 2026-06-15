ecf5118f223f50e23866610d7bb9ed41aea4f43d
Alex Han <alexhzc100318@gmail.com>
2026-06-14 22:34:29 -0600

    Update #02_Odd_or_Even.py

--- #02_Odd_or_Even.py
+++ #02_Odd_or_Even.py
@@ -1,3 +1,8 @@
+#The exercise comes first (with a few extras if you want the extra challenge or want to spend more time), followed by a discussion. Enjoy!
+#Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+
+
 # State: Program determines whether the ineger is even or odd
 # Input: An ieteger enterd by the user
 # Rule:
